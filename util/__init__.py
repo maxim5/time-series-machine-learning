921d30b312cf704d3ec06934ff9a2fccdae427c6
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-10 21:57:04 +0200

    minor

--- util/__init__.py
+++ util/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'maxim'
 
-from collection_util import smart_str
+from collection_util import smart_str, str_to_obj
 from data_set import DataSet
 from data_util import read_df, to_changes, split_train_test, to_dataset
 from logging import debug, info, warn, vlog, vlog2, vlog3
