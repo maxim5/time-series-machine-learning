aa276301f3ccbb3b3e0e59c8f231f9d71dcd2bf5
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-12 15:02:42 +0200

    NN model: restore

--- models/model.py
+++ models/model.py
@@ -5,7 +5,7 @@ __author__ = 'maxim'
 
 class Model:
   def __init__(self, **params):
-    pass
+    self._features = params['features']
 
   def session(self):
     class Dummy:
@@ -13,14 +13,14 @@ class Model:
       def __exit__(self, exc_type, exc_val, exc_tb): pass
     return Dummy()
 
-
   def fit(self, train_set):
     raise NotImplementedError
-
 
   def predict(self, x):
     raise NotImplementedError
 
-
   def save(self, dest_dir):
+    pass
+
+  def restore(self, source_dir):
     pass
--- models/nn_model.py
+++ models/nn_model.py
@@ -22,49 +22,61 @@ class NeuralNetworkModel(Model):
     self._lambda = params.get('lambda', 0.005)
     self._dropout = params.get('dropout', 0.5)
 
+    self._graph = None
     self._session = None
+    self._compile()
+
+
+  def _compile(self):
+    with tf.Graph().as_default() as self._graph:
+      x = tf.placeholder(tf.float32, shape=[None, self._features], name='x')
+      y = tf.placeholder(tf.float32, shape=[None], name='y')
+      mode = tf.placeholder(tf.string, name='mode')
+
+      init = lambda shape: tf.random_normal(shape=shape) * self._init_sigma
+
+      W1 = tf.Variable(init([self._features, self._hidden_layer]), name='W1')
+      b1 = tf.Variable(init([self._hidden_layer]), name='b1')
+      layer1 = tf.matmul(x, W1) + b1
+      layer1 = tf.nn.relu(layer1)
+      layer1 = dropout(layer1, tf.equal(mode, 'train'), keep_prob=self._dropout)
+
+      W2 = tf.Variable(init([self._hidden_layer, 1]), name='W2')
+      b2 = tf.Variable(init([1]), name='b2')
+      output_layer = tf.matmul(layer1, W2) + b2
+
+      reg = self._lambda * (tf.nn.l2_loss(W1) + tf.nn.l2_loss(W2))
+      cost = tf.reduce_mean(tf.abs(output_layer - y)) + reg
+      optimizer = tf.train.AdamOptimizer(self._learning_rate).minimize(cost)
+
+      init = tf.global_variables_initializer()
+
+    self._x = x
+    self._y = y
+    self._mode = mode
+    self._output_layer = output_layer
+    self._cost = cost
+    self._optimizer = optimizer
+    self._init = init
 
 
   def session(self):
-    self._session = tf.Session()
+    assert self._graph is not None
+    self._session = tf.Session(graph=self._graph)
     return self._session
 
 
   def fit(self, train):
-    _, features = train.x.shape
-
-    x = tf.placeholder('float', shape=[None, features], name='x')
-    y = tf.placeholder('float', shape=[None], name='y')
-    mode = tf.placeholder(tf.string, name='mode')
-
-    W1 = tf.Variable(tf.random_normal(shape=[features, self._hidden_layer]) * self._init_sigma, name='W1')
-    b1 = tf.Variable(tf.random_normal(shape=[self._hidden_layer]) * self._init_sigma, name='b1')
-    layer1 = tf.matmul(x, W1) + b1
-    layer1 = tf.nn.relu(layer1)
-    layer1 = dropout(layer1, tf.equal(mode, 'train'), keep_prob=self._dropout)
-
-    W2 = tf.Variable(tf.random_normal(shape=[self._hidden_layer, 1]) * self._init_sigma, name='W2')
-    b2 = tf.Variable(tf.random_normal(shape=[1]) * self._init_sigma, name='b2')
-    output_layer = tf.matmul(layer1, W2) + b2
-
-    reg = self._lambda * (tf.nn.l2_loss(W1) + tf.nn.l2_loss(W2))
-    cost = tf.reduce_mean(tf.abs(output_layer - y)) + reg
-    optimizer = tf.train.AdamOptimizer(self._learning_rate).minimize(cost)
-
-    init = tf.global_variables_initializer()
-
+    assert self._session is not None
     debug('Start training')
-    self._session.run(init)
+    self._session.run(self._init)
     while train.epochs_completed < self._epochs:
       batch_x, batch_y = train.next_batch(self._batch_size)
-      _, cost_ = self._session.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y, mode: 'train'})
+      _, cost_ = self._session.run([self._optimizer, self._cost],
+                                   feed_dict={self._x: batch_x, self._y: batch_y, self._mode: 'train'})
       if train.just_completed and train.epochs_completed % 10 == 0:
         info('Epoch: %2d cost=%.6f' % (train.epochs_completed, cost_))
     debug('Training completed')
-
-    self._x = x
-    self._mode = mode
-    self._output_layer = output_layer
 
 
   def predict(self, test_x):
@@ -77,6 +89,13 @@ class NeuralNetworkModel(Model):
     saver = tf.train.Saver()
     saver.save(self._session, path)
     info('Session saved to %s' % path)
+
+
+  def restore(self, source_dir):
+    path = os.path.join(source_dir, 'session.data')
+    saver = tf.train.Saver()
+    saver.restore(self._session, path)
+    info('Session restored from %s' % path)
 
 
 def dropout(incoming, is_training, keep_prob):
