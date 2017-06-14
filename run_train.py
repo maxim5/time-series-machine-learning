f021f1a59fed7bc04d02c12740a8a40296d97a76
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-14 10:29:50 +0200

    training spec

--- run_train.py
+++ run_train.py
@@ -13,18 +13,19 @@ def main():
   for ticker in ['BTC_ETH_2h', 'BTC_DGB_2h']:
     for target in ['high']:
       job_info = JobInfo('_data', '_zoo', ticker=ticker, target=target)
-      job_runner = JobRunner(job_info, limit=0.2)
-      job_runner.iterate(iterations=30, params_fun=lambda : {
+      job_runner = JobRunner(job_info, limit='auto')
+      job_runner.iterate(iterations=10, params_fun=lambda : {
         'target': job_info.target,
-        'k': np.random.choice([1, 2, 3, 4, 5, 6, 7, 8]),
+        'k': np.random.choice(range(1, 9)),
         'model_class': NeuralNetworkModel,
         'model_params': {
-          'batch_size': np.random.choice([200, 500, 1000, 2000]),
+          'batch_size': np.random.choice([1000, 2000, 4000]),
           'epochs': 100,
           'hidden_layer': np.random.randint(50, 400),
-          'learning_rate': 10 ** np.random.uniform(-0.5, 0.5),
+          'learning_rate': 10 ** np.random.uniform(-2.0, -0.0),
           'init_sigma': 10 ** np.random.uniform(-8, -2),
-          'lambda': 10 ** np.random.uniform(-11, -6),
+          'cost_func': np.random.choice(['l1', 'l2']),
+          'lambda': 10 ** np.random.uniform(-12, -6),
           'dropout': np.random.uniform(0.1, 0.9),
         }
       })
