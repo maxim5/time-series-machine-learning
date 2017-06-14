b492c3e0a65d1ed479de64030beca29a80b3462e
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-14 11:10:04 +0200

    a bigger spec to find the best model

--- run_train.py
+++ run_train.py
@@ -13,17 +13,18 @@ def main():
   for ticker in ['BTC_ETH_2h', 'BTC_DGB_2h']:
     for target in ['high']:
       job_info = JobInfo('_data', '_zoo', ticker=ticker, target=target)
-      job_runner = JobRunner(job_info, limit=np.mean)
+      job_runner = JobRunner(job_info, limit=np.min)
       job_runner.iterate(iterations=10, params_fun=lambda : {
         'target': job_info.target,
         'k': np.random.choice(range(1, 9)),
         'model_class': NeuralNetworkModel,
         'model_params': {
-          'batch_size': np.random.choice([1000, 2000, 4000]),
+          'batch_size': np.random.choice([1000, 2000, 3000, 4000]),
           'epochs': 100,
-          'hidden_layer': np.random.randint(50, 400),
+          'hidden_layer': np.random.randint(40, 500),
           'learning_rate': 10 ** np.random.uniform(-2.0, 0.5),
           'init_sigma': 10 ** np.random.uniform(-8, -2),
+          'activation_func': np.random.choice(['relu', 'elu', 'sigmoid', 'leaky_relu', 'prelu']),
           'cost_func': np.random.choice(['l1', 'l2']),
           'lambda': 10 ** np.random.uniform(-12, -6),
           'dropout': np.random.uniform(0.1, 0.9),
