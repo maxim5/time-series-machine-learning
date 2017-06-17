bb9cc11402ebba27aac9c96508f060c22b4f06f9
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-17 12:32:56 +0200

    update the predictor

--- run_predict.py
+++ run_predict.py
@@ -35,12 +35,12 @@ def predict_model(changes_df, path):
   model_info = get_model_info(path)
   run_params = model_info.run_params
   model = model_info.model_class(**model_info.model_params)
-  data_set = to_dataset(changes_df, run_params['k'], run_params['target'], model_info.model_class.DATA_WITH_BIAS)
-  data_set.x = data_set.x[-1:]
+  x = to_dataset_for_prediction(changes_df[:-1], run_params['k'], model_info.model_class.DATA_WITH_BIAS)
+  x = x[-1:]
 
   with model.session():
     model.restore(model_info.path)
-    predicted = float(model.predict(data_set.x))
+    predicted = float(model.predict(x))
     info('Predicted change=%.5f' % predicted)
     return predicted
 
@@ -53,13 +53,14 @@ def predict_all_models(changes_df, name, accept):
     value = predict_model(changes_df, os.path.join(home_dir, model))
     predictions.append(value)
   info()
-  info('Mean predicted value: %.5f' % np.mean(predictions))
+  info('Mean predicted value for %s: %.5f' % (name, np.mean(predictions)))
+  info()
 
 
 def main():
-  period = '2h'
-  for ticker in ['BTC_ETH', 'BTC_DGB']:
-    raw_df = poloniex.get_latest_data(ticker, period=period, depth=10)
+  period = 'day'
+  for ticker in ['BTC_ETH', 'BTC_DGB', 'BTC_ZEC', 'BTC_STR']:
+    raw_df = poloniex.get_latest_data(ticker, period=period, depth=25)
     changes_df = to_changes(raw_df)
     predict_all_models(changes_df, '%s_%s' % (ticker, period), lambda name: name.startswith('high_'))
 
