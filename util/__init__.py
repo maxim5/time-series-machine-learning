4017dc6150549f1535d95dd6c81699e2c69a4452
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-17 12:19:27 +0200

    add to __init__

--- util/__init__.py
+++ util/__init__.py
@@ -3,5 +3,5 @@ __author__ = 'maxim'
 
 from collection_util import smart_str, str_to_obj
 from data_set import DataSet
-from data_util import read_df, to_changes, to_dataset, split_dataset
+from data_util import read_df, to_changes, to_dataset, to_dataset_for_prediction, split_dataset
 from logging import debug, info, warn, vlog, vlog2, vlog3
