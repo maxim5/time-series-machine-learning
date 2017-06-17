97d16d95d37e08c478efaac08c4448103ae0d34c
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2017-06-17 12:30:12 +0200

    minor

--- run_fetch.py
+++ run_fetch.py
@@ -7,7 +7,7 @@ import poloniex
 
 
 def main():
-  poloniex.update_selected(tickers=['BTC_ETH', 'BTC_DGB', 'BTC_STR'])
+  poloniex.update_selected(tickers=['BTC_ETH', 'BTC_DGB', 'BTC_STR', 'BTC_ZEC'])
 
 
 if __name__ == '__main__':
