de246c717d8fa43b568384f34610997f6d51d2bb
Maxim Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-02-04 23:43:39 +0100

    minor

--- util/collection_util.py
+++ util/collection_util.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def smart_str(val):
   if type(val) in [float, np.float32, np.float64] and val:
-    return "%.6f" % val if abs(val) > 1e-6 else "%e" % val
+    return '%.6f' % val if abs(val) > 1e-6 else '%e' % val
   if type(val) == dict:
     return '{%s}' % ', '.join(['%s: %s' % (repr(k), smart_str(val[k])) for k in sorted(val.keys())])
   if type(val) in [list, tuple]:
